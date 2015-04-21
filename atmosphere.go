2d3f04aacd2494e1d9d32341143d877c24a721a9
G. Hussain Chinoy <ghchinoy@gmail.com>
2015-04-21 16:11:42 -0600

    less upload, separated out zip methods into a package

--- atmosphere.go
+++ atmosphere.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"archive/zip"
-	"bufio"
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
-	"strings"
+	"path/filepath"
 
+	"bytes"
+
+	"bitbucket.org/ghchinoy/atmotool/zip"
 	"github.com/docopt/docopt-go"
 )
 
 // Configuration
 type Configuration struct {
 	Url      string `json:"url"`
-	Username string `json:"username"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type Auth struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var config Configuration
+var client *http.Client
 
 func main() {
 
@@ -62,7 +75,7 @@ Options:
 		flag.Usage()
 		os.Exit(1)
 	}
-	var config Configuration
+	//var config Configuration
 	err = json.Unmarshal(configBytes, &config)
 	if err != nil {
 		fmt.Printf("Unable to parse configuration file. %s\n", err)
@@ -92,148 +105,149 @@ Options:
 	} else if arguments["zip"] == true {
 		prefix, _ := arguments["<prefix>"].(string)
 		dir, _ := arguments["--dir"].(string)
-		zipPredefinedPath(prefix, dir)
+		zip.ZipPredefinedPath(prefix, dir)
 	}
-}
-
-func zipPredefinedPath(prefix string, dir string) {
-	//fmt.Printf("Zipping '%s' with prefix '%s'\n", dir, prefix)
-
-	// test to see if expected paths exist
-	resources_dir := dir + "/resources/theme/default"
-	content_dir := dir + "/landing"
-
-	resources_src, err := os.Stat(resources_dir)
-	if err != nil {
-		fmt.Printf("Error with resource dir ", err)
-		os.Exit(1)
-	}
-	if !resources_src.IsDir() {
-		fmt.Printf("%s is not a directory.", resources_dir)
-	}
-
-	content_src, err := os.Stat(content_dir)
-	if err != nil {
-		fmt.Printf("Error with content dir ", err)
-		os.Exit(1)
-	}
-	if !content_src.IsDir() {
-		fmt.Printf("%s is not a directory.", content_dir)
-		os.Exit(1)
-	}
-
-	exclusions := []string{".DS_Store", ".zip"}
-
-	// Get the file lists
-	resourcesFileList := listFilesInDir(resources_dir, "", exclusions, false)
-	contentFileList := listFilesInDir(content_dir, "", exclusions, false)
-
-	// Create zip files
-	writeZipTo(createZipBuffer(resourcesFileList), prefix+"_resourcesThemeDefault.zip")
-	zipTheseFiles(contentFileList, prefix+"_contentHomeLanding.zip")
-}
-
-// writes a zip buffer to a filename
-func writeZipTo(zipbuffer *bytes.Buffer, filename string) {
-
-	fout, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := fout.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	fw := bufio.NewWriter(fout)
-	fw.Write(zipbuffer.Bytes())
-	fw.Flush()
-
-	fileinfo, _ := os.Stat(filename)
-	fmt.Printf("Created %s (%v)\n", filename, fileinfo.Size())
-
-}
-
-// creates a zip byte buffer and returns
-func createZipBuffer(filesList []string) *bytes.Buffer {
-
-	buf := new(bytes.Buffer)
-	w := zip.NewWriter(buf)
-	for _, file := range filesList {
-		f, err := w.Create(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		byts, _ := ioutil.ReadFile(file)
-		_, err = f.Write(byts)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Make sure to check the error on Close.
-	err := w.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return buf
-
-}
-
-// convenience method
-func zipTheseFiles(filesList []string, filename string) {
-
-	writeZipTo(createZipBuffer(filesList), filename)
-
-}
-
-// creates a list of file paths suitable for zipping
-func listFilesInDir(path string, relpath string, exclusions []string, debug bool) []string {
-
-	if debug {
-		fmt.Printf("%s\n", path)
-	}
-
-	var fileList []string
-
-	files, _ := ioutil.ReadDir(path)
-	for _, f := range files {
-
-		exclude := false
-		for _, e := range exclusions {
-			if exclude = strings.Contains(f.Name(), e); exclude {
-				break
-			}
-		}
-
-		if debug && exclude {
-			fmt.Printf("  - %s\n", f.Name())
-		}
-
-		if !exclude {
-			if debug {
-				fmt.Printf("   %s\n", f.Name())
-			}
-			if f.IsDir() {
-				dirFileList := listFilesInDir(path+"/"+f.Name(), f.Name()+"/", exclusions, debug)
-				fileList = append(fileList, dirFileList...)
-			}
-			fileList = append(fileList, relpath+f.Name())
-		}
-
-	}
-
-	return fileList
 }
 
 // Convenience method
 func uploadLessFile(lessFilePath string, config Configuration) {
-	fmt.Printf("Uploading Less file %s to %s\n", lessFilePath, config.Url)
-	// upload to CMS path ??
-	// call rebuild styles API
+	log.Printf("Uploading Less file %s to %s\n", lessFilePath, config.Url)
 
+	err := loginToCM()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	//
+	// Upload
+	log.Println("Uploading custom.less ...")
+	extraParams := map[string]string{
+		"none": "really",
+	}
+	lessUploadUri := config.Url + "/resources/theme/default/less?unpack=false"
+	var request *http.Request
+	request, err = newFileUploadRequest(lessUploadUri, extraParams, "File", lessFilePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		body := &bytes.Buffer{}
+		_, err := body.ReadFrom(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		resp.Body.Close()
+		log.Printf("Upload status %v", resp.StatusCode)
+	}
+
+	if resp.StatusCode == 200 {
+		err = rebuildStyles("default")
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+}
+
+func loginToCM() error {
+	// Login
+	log.Println("Logging in...")
+	client = &http.Client{}
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatalln(err)
+		return err
+	}
+	client.Jar = jar
+
+	loginUri := config.Url + "/api/login"
+	auth := Auth{config.Email, config.Password}
+	buf, err := json.Marshal(auth)
+	if err != nil {
+		log.Fatalln(err)
+		return err
+	}
+	req, err := http.NewRequest("POST", loginUri, bytes.NewReader(buf))
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+		return err
+	}
+	log.Printf("Login %s", resp.Status)
+
+	return nil
+
+}
+
+func rebuildStyles(theme string) error {
+
+	// call rebuild styles API
+	// POST CM_URI/resources/branding/generatestyles
+	// Form Data
+	// theme: default
+	log.Println("Rebuilding styles...")
+	rebuildStylesUri := config.Url + "/resources/branding/generatestyles"
+	resp, err := http.PostForm(rebuildStylesUri, url.Values{"theme": {theme}})
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+		return err
+	}
+	var results map[string]interface{}
+	err = json.Unmarshal(data, &results)
+	status := results["result"]
+	log.Printf("Rebuild styles: %s", status)
+	return nil
+}
+
+func newFileUploadRequest(uri string, params map[string]string, paramName string, path string) (*http.Request, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	//writer.SetBoundary("WebKitFormBoundaryoa9Fs4QksdotlWrl")
+
+	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = io.Copy(part, file)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+
+	/*
+		for key, val := range params {
+			_ = writer.WriteField(key, val)
+		}
+	*/
+
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, _ := http.NewRequest("POST", uri, body)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	return req, nil
 }
 
 // Convenience method
