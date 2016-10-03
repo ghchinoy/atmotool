3aa564391bd620faf5d542a8add1fbb51b1acb54
G. Hussain Chinoy <ghchinoy@gmail.com>
2016-10-02 18:44:56 -0600

    cms package, anticipating other create methods needing these

---
+++ cms/add.go
@@ -0,0 +1,9 @@
+package cms
+
+import "github.com/ghchinoy/atmotool/control"
+
+// AddFiletoCMS adds a file to the platform CMS
+func AddFileToCMS(filepath string, config control.Configuration, debug bool) error {
+
+	return nil
+}
