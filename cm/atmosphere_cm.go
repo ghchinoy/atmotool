04a1062d3b524e72ddf811965a595789b4c0c5b8
G. Hussain Chinoy <ghchinoy@gmail.com>
2016-11-06 15:18:13 -0700

    updated wiht more info for an api

--- cm/atmosphere_cm.go
+++ cm/atmosphere_cm.go
@@ -13,14 +13,18 @@ type Channel struct {
 
 // Item is the generic container
 type Item struct {
-	Title            string            `json:"title"`
-	Description      string            `json:"description"`
-	Category         []ValueDomain     `json:"category"`
-	Guid             Guid              `json:"guid"`
-	PubDate          string            `json:"pubDate"`
-	EntityReferences []EntityReference `json:"EntityReferences.EntityReference"`
-	EntityReference  EntityReference   `json:"EntityReference"`
-	ImageUrl         string            `json:"Image.Url"`
+	Title       string        `json:"title"`
+	Description string        `json:"description"`
+	Category    []ValueDomain `json:"category"`
+	Guid        Guid          `json:"guid"`
+	PubDate     string        `json:"pubDate"`
+	// Note, the XML field tag notation does not work for JSON
+	//EntityReferences []EntityReference `json:"EntityReferences.EntityReference"`
+	EntityReferences struct {
+		EntityReference []EntityReference
+	}
+	EntityReference EntityReference `json:"EntityReference"`
+	ImageUrl        string          `json:"Image.Url"`
 	// App
 	Connections int
 	Followers   int
@@ -35,6 +39,10 @@ type Item struct {
 	CommentsCount int
 	GroupsCount   int
 	Domain        string
+	//Endpoints     []Endpoint `json:"Endpoints.Endpoint"`
+	Endpoints struct {
+		Endpoint []Endpoint
+	}
 }
 
 // ValueDomain is a key - value pair
@@ -51,6 +59,41 @@ type Guid struct {
 // EntityReference is a reference to another entity
 type EntityReference struct {
 	Title    string
-	Guid     string
+	Guid     string `json:"Guid"`
 	Category ValueDomain
+}
+
+// Endpoint is a structure of an endpoint
+type Endpoint struct {
+	BindingQName         string
+	BindingType          string
+	CName                string
+	Category             string
+	ConnectionPorperties []ValueDomain
+	DeploymentZoneRule   string
+	//EndpointImplementationDetails DeploymentZoneEndpoint `json:"EndpointImplementationDetails.DeploymentZoneEndpoint"`
+	EndpointImplementationDetails struct {
+		DeploymentZoneEndpoint
+	}
+	EndpointKey        string
+	ImplementationCode string
+	URI                string `json:"Uri"`
+}
+
+// DeploymentZoneEndpoint contains information about a Deployment Zone Endpoint
+type DeploymentZoneEndpoint struct {
+	BindingQName     string
+	BindingType      string
+	ContainerKey     string
+	DeploymentZoneID string
+	EndpointHostname string
+	EndpointKey      string
+	EndpointPath     string
+	GatewayHostName  string
+	GatewayHostPath  string
+	ListenerName     string
+	Path             string
+	Protocol         string
+	Public           bool
+	URL              string `json:"Url"`
 }
