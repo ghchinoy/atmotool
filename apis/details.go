33c999be08e47177aa53b00aa7fff84e1d482a1c
G. Hussain Chinoy <ghchinoy@gmail.com>
2016-11-07 10:39:29 -0700

    stubbing out api details

--- apis/details.go
+++ apis/details.go
@@ -3,7 +3,8 @@ package apis
 const (
 	// APIGetInfo is the endpoint to get info about an API
 	APIGetInfo = "/api/apis/%s"
-	// APIGetVersionInfo is the endpoint pattern for getting information about a version
-	APIGetVersionInfo         = "/api/apis/versions/%s"
+	// APIGetVersionInfo is the endpoint pattern for getting information about a version, defaulting endpoint enclusion
+	APIGetVersionInfo = "/api/apis/versions/%s?IncludeEndpoints=true"
+	// APIVersionImplementations is the endpoint for getting implementation info
 	APIVersionImplementations = "/api/apis/versions/implementations"
 )
